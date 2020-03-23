d8244d195aba3d794afdee1044c4a722e81cde99
Alsoxavi <oscarj9410@gmail.com>
2020-03-23 13:05:22 -0500

    Added new fonts and start navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 import './screens/categories_screen.dart';
@@ -11,7 +10,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
+        accentColor: Color.fromRGBO(9, 112, 84, 1), //Color.fromRGBO(101, 153, 255, 1),
+        canvasColor: Color.fromRGBO(255,254,229,1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              body2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              title: TextStyle(
+                fontFamily: 'RobotoCondensed',
+                fontSize: 25,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
       ),
       home: CategoriesScreen(),
     );
